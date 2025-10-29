d291af2cbe1dd1aa084487e9e0aafa8c05821c9d
privacylab31 <privacylab31@gmail.com>
2025-10-30 02:29:01 +0530

    Add consent banner for cookie usage

---
+++ inc/consent-banner.php
@@ -0,0 +1,9 @@
+<div id="consent-banner" class="consent-banner" style="display:none;">
+  <div class="consent-inner">
+    <p>This site uses test cookies to simulate Magento behavior. <a href="privacy.php">Learn more</a></p>
+    <div class="consent-actions">
+      <button id="accept-all">Accept all</button>
+      <button id="reject-nonessential">Reject non-essential</button>
+    </div>
+  </div>
+</div>
