be64dd844bade31771f031da751c762e327c95fe
privacylab31 <privacylab31@gmail.com>
2025-10-30 02:26:04 +0530

    Add links to category, cart, and checkout pages

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ include 'inc/header.php';
   <p>Welcome! This small demo helps test cookie behavior and consent handling.</p>
   <ul>
     <li><a href="product.php">Product Page</a></li>
+    <li><a href="category.php">Category Page</a></li>
+    <li><a href="cart.php">Cart Page</a></li>
+    <li><a href="checkout.php">Checkout Page</a></li>
     <li><a href="account.php">Account Page</a></li>
     <li><a href="privacy.php">Privacy Page</a></li>
     <li><a href="contact.php">Contact Page</a></li>
